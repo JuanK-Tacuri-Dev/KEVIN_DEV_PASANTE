9e8cb0023bd9e16b14613c1372a982481edd7c12
andresfgonzalez <infraestructura-qs@hotmail.com>
2024-06-20 15:40:07 -0500

    avance tests

---
+++ Integration.Orchestrator.Backend.Api.Tests/Controllers/v1/Administration/IntegrationControllerTests.cs
@@ -0,0 +1,183 @@
+﻿using Integration.Orchestrator.Backend.Api.Controllers.v1.Administration;
+using Integration.Orchestrator.Backend.Application.Models.Administration.Integration;
+using Integration.Orchestrator.Backend.Domain.Resources;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using static Integration.Orchestrator.Backend.Application.Handlers.Administration.Integration.IntegrationCommands;
+
+namespace Integration.Orchestrator.Backend.Api.Tests.Controllers.v1.Administration
+{
+    public class IntegrationControllerTests
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly IntegrationController _controller;
+
+        public IntegrationControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _controller = new IntegrationController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task Create_ReturnsOkResult()
+        {
+            // Arrange
+            var request = new IntegrationCreateRequest()
+            {
+                Name = "Test",
+                Status = Guid.NewGuid(),
+                Observations = "observation",
+                UserId = Guid.NewGuid(),
+                Process = new List<ProcessRequest>
+                {
+                    new ProcessRequest
+                    {
+                        Id = Guid.NewGuid()
+                    }
+                }
+            };
+            var command = new CreateIntegrationCommandRequest(new IntegrationBasicInfoRequest<IntegrationCreateRequest>(request));
+
+            var response = new CreateIntegrationCommandResponse(
+                new IntegrationCreateResponse
+                {
+                    Code = 200,
+                    Description = AppMessages.Application_IntegrationResponseCreated,
+                    Data = new IntegrationCreate
+                    {
+                        Id = Guid.NewGuid()
+                    }
+                });
+            _mediatorMock.Setup(m => m.Send(It.IsAny<CreateIntegrationCommandRequest>(), default))
+                         .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.Create(request);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<CreateIntegrationCommandResponse>(okResult.Value);
+            Assert.Equal(200, returnValue.Message.Code);
+            Assert.Equal(AppMessages.Application_IntegrationResponseCreated, returnValue.Message.Description);
+            _mediatorMock.Verify(m => m.Send(It.IsAny<CreateIntegrationCommandRequest>(), default), Times.Once);
+        }
+
+        [Fact]
+        public async Task Update_ReturnsOkResult()
+        {
+            // Arrange
+            var request = new IntegrationUpdateRequest
+            {
+                Name = "Test",
+                Status = Guid.NewGuid(),
+                Observations = "observation",
+                UserId = Guid.NewGuid(),
+                Process = new List<ProcessRequest>
+                {
+                    new ProcessRequest
+                    {
+                        Id = Guid.NewGuid()
+                    }
+                }
+            };
+            var id = Guid.NewGuid();
+
+            var response = new UpdateIntegrationCommandResponse(
+                new IntegrationUpdateResponse
+                {
+                    Code = 200,
+                    Description = AppMessages.Application_IntegrationResponseUpdated,
+                    Data = new IntegrationUpdate
+                    {
+                        Id = id
+                    }
+                });
+
+            _mediatorMock.Setup(m => m.Send(It.IsAny<UpdateIntegrationCommandRequest>(), default))
+                         .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.Update(request, id);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<UpdateIntegrationCommandResponse>(okResult.Value);
+            Assert.Equal(200, returnValue.Message.Code);
+            Assert.Equal(AppMessages.Application_IntegrationResponseUpdated, returnValue.Message.Description);
+        }
+
+        [Fact]
+        public async Task Delete_ReturnsOkResult()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var response = new DeleteIntegrationCommandResponse(
+                new IntegrationDeleteResponse
+                {
+                    Code = 200,
+                    Description = AppMessages.Application_IntegrationResponseDeleted
+                });
+
+            _mediatorMock.Setup(m => m.Send(It.IsAny<DeleteIntegrationCommandRequest>(), default))
+                         .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.Delete(id);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<DeleteIntegrationCommandResponse>(okResult.Value);
+            Assert.Equal(200, returnValue.Message.Code);
+            Assert.Equal(AppMessages.Application_IntegrationResponseDeleted, returnValue.Message.Description);
+        }
+        
+        [Fact]
+        public async Task GetAllPaginated_ReturnsOkResult()
+        {
+            // Arrange
+            var request = new IntegrationGetAllPaginatedRequest
+            {
+                Page = 1,
+                Rows = 1,
+                SortBy = ""
+            };
+            var response = new GetAllPaginatedIntegrationCommandResponse(
+                new IntegrationGetAllPaginatedResponse
+                {
+                    Code = 200,
+                    Description = AppMessages.Api_IntegrationResponse,
+                    TotalRows = 1,
+                    Data = [
+                        new IntegrationGetAllPaginated
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = "Test",
+                            Status = Guid.NewGuid(),
+                            Observations = "observation",
+                            UserId = Guid.NewGuid(),
+                            Process = new List<ProcessRequest>
+                            {
+                                new ProcessRequest
+                                {
+                                    Id = Guid.NewGuid()
+                            }
+                                }
+                        }]
+                });
+            var command = new GetAllPaginatedIntegrationCommandRequest(request);
+            _mediatorMock.Setup(m => m.Send(It.IsAny<GetAllPaginatedIntegrationCommandRequest>(), default))
+                         .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAllPaginated(request);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<GetAllPaginatedIntegrationCommandResponse>(okResult.Value);
+            Assert.Equal(200, returnValue.Message.Code);
+            Assert.Equal(AppMessages.Api_IntegrationResponse, returnValue.Message.Description);
+            _mediatorMock.Verify(m => m.Send(It.IsAny<GetAllPaginatedIntegrationCommandRequest>(), default), Times.Once);
+        }
+    }
+}
---
+++ Integration.Orchestrator.Backend.Application.Tests/Administrations/Handlers/Administration/Integration/Validators/CreateIntegrationCommandRequestValidatorTests.cs
@@ -0,0 +1,118 @@
+﻿using FluentValidation.TestHelper;
+using Integration.Orchestrator.Backend.Application.Models.Administration.Integration;
+using static Integration.Orchestrator.Backend.Application.Handlers.Administration.Integration.IntegrationCommands;
+
+namespace Integration.Orchestrator.Backend.Application.Tests.Administrations.Handlers.Administration.Integration.Validators
+{
+    public class CreateIntegrationCommandRequestValidatorTests
+    {/*
+        private readonly CreateIntegrationCommandRequestValidator _validator;
+
+        public CreateIntegrationCommandRequestValidatorTests()
+        {
+            _validator = new CreateIntegrationCommandRequestValidator();
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Code_Is_Null_Or_Empty()
+        {
+            // Arrange
+            var request = new CreateIntegrationCommandRequest(new IntegrationBasicInfoRequest<IntegrationCreateRequest>(new IntegrationCreateRequest 
+            { 
+                Code = string.Empty 
+            }));
+
+            // Act
+            var result = _validator.TestValidate(request);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(r => r.Integration.IntegrationRequest.Code)
+                  .WithErrorMessage("AppMessages.Integration_Code_Required");
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Server_Is_Null_Or_Empty()
+        {
+            // Arrange
+            var request = new CreateIntegrationCommandRequest(new IntegrationBasicInfoRequest<IntegrationCreateRequest>(new IntegrationCreateRequest 
+            { 
+                Server = string.Empty 
+            }));
+
+            // Act
+            var result = _validator.TestValidate(request);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(r => r.Integration.IntegrationRequest.Server)
+                  .WithErrorMessage("AppMessages.Integration_Server_Required");
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Port_Is_Null_Or_Empty()
+        {
+            // Arrange
+            var request = new CreateIntegrationCommandRequest(new IntegrationBasicInfoRequest<IntegrationCreateRequest>(new IntegrationCreateRequest 
+            { 
+                Port = string.Empty 
+            }));
+
+            // Act
+            var result = _validator.TestValidate(request);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(r => r.Integration.IntegrationRequest.Port)
+                  .WithErrorMessage("AppMessages.Integration_Port_Required");
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_User_Is_Null_Or_Empty()
+        {
+            // Arrange
+            var request = new CreateIntegrationCommandRequest(new IntegrationBasicInfoRequest<IntegrationCreateRequest>(new IntegrationCreateRequest 
+            { 
+                User = string.Empty 
+            }));
+
+            // Act
+            var result = _validator.TestValidate(request);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(r => r.Integration.IntegrationRequest.User)
+                  .WithErrorMessage("AppMessages.Integration_User_Required");
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Password_Is_Null_Or_Empty()
+        {
+            // Arrange
+            var request = new CreateIntegrationCommandRequest(new IntegrationBasicInfoRequest<IntegrationCreateRequest>(new IntegrationCreateRequest 
+            { 
+                Password = string.Empty 
+            }));
+            
+            // Act
+            var result = _validator.TestValidate(request);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(r => r.Integration.IntegrationRequest.Password)
+                  .WithErrorMessage("AppMessages.Integration_Password_Required");
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Adapter_Is_Null_Or_Empty()
+        {
+            // Arrange
+            var request = new CreateIntegrationCommandRequest(new IntegrationBasicInfoRequest<IntegrationCreateRequest>(new IntegrationCreateRequest 
+            { 
+                Adapter = string.Empty 
+            }));
+
+            // Act
+            var result = _validator.TestValidate(request);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(r => r.Integration.IntegrationRequest.Adapter)
+                  .WithErrorMessage("AppMessages.Integration_Adapter_Required");
+        }*/
+    }
+}
