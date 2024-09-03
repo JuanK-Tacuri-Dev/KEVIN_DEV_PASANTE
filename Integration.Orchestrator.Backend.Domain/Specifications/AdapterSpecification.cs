8f80f5f40acfa898c01e1e33f80d30bef6b804e8
andresfgonzalez <infraestructura-qs@hotmail.com>
2024-09-03 12:03:57 -0500

    Se resuelve bug ajustando especificación realizando la busqueda por name y ordendando por name y code.

--- Integration.Orchestrator.Backend.Domain/Specifications/AdapterSpecification.cs
+++ Integration.Orchestrator.Backend.Domain/Specifications/AdapterSpecification.cs
@@ -1,7 +1,7 @@
-﻿using System.Linq.Expressions;
-using Integration.Orchestrator.Backend.Domain.Commons;
+﻿using Integration.Orchestrator.Backend.Domain.Commons;
 using Integration.Orchestrator.Backend.Domain.Entities.Administration;
 using Integration.Orchestrator.Backend.Domain.Models;
+using System.Linq.Expressions;
 
 namespace Integration.Orchestrator.Backend.Domain.Specifications
 {
@@ -10,13 +10,13 @@ namespace Integration.Orchestrator.Backend.Domain.Specifications
         public Expression<Func<AdapterEntity, bool>> Criteria { get; private set; }
 
         public Expression<Func<AdapterEntity, object>> OrderBy { get; private set; }
-        
+
         public Expression<Func<AdapterEntity, object>> OrderByDescending { get; private set; }
 
         public int Skip { get; private set; }
 
         public int Limit { get; private set; }
-       
+
         public AdapterSpecification(PaginatedModel paginatedModel)
         {
             Criteria = BuildCriteria(paginatedModel);
@@ -24,11 +24,11 @@ namespace Integration.Orchestrator.Backend.Domain.Specifications
             SetupOrdering(paginatedModel);
         }
 
-        private static readonly Dictionary<string, Expression<Func<AdapterEntity, object>>> sortExpressions 
+        private static readonly Dictionary<string, Expression<Func<AdapterEntity, object>>> sortExpressions
             = new Dictionary<string, Expression<Func<AdapterEntity, object>>>
         {
-            { nameof(AdapterEntity.adapter_name), x => x.adapter_name },
-            { nameof(AdapterEntity.adapter_code), x => x.adapter_code },
+            { nameof(AdapterEntity.adapter_name).Split("_")[1], x => x.adapter_name },
+            { nameof(AdapterEntity.adapter_code).Split("_")[1], x => x.adapter_code },
             { nameof(AdapterEntity.created_at), x => x.created_at }
         };
         private void SetupPagination(PaginatedModel model)
@@ -72,7 +72,6 @@ namespace Integration.Orchestrator.Backend.Domain.Specifications
             if (!string.IsNullOrEmpty(search))
             {
                 criteria = criteria.And(x =>
-                x.adapter_code.ToUpper().Contains(search.ToUpper()) ||
                 x.adapter_name.ToLower().Contains(search.ToLower()));
             }
 
