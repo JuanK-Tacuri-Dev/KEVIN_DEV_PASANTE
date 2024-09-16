b49bc84e20ecde119813913b5494f9acd255d853
andresfgonzalez <infraestructura-qs@hotmail.com>
2024-09-16 17:16:11 -0500

    ajuste validación detalle

--- Integration.Orchestrator.Backend.Application/Handlers/Administration/Catalog/Validators/UpdateCatalogCommandRequestValidator.cs
+++ Integration.Orchestrator.Backend.Application/Handlers/Administration/Catalog/Validators/UpdateCatalogCommandRequestValidator.cs
@@ -17,7 +17,7 @@ namespace Integration.Orchestrator.Backend.Application.Handlers.Administration.C
            .MaximumLength(150).WithMessage(string.Format(AppMessages.Application_Validator_MaxLength, 150));
 
             RuleFor(request => request.Catalog.CatalogRequest.Detail)
-             .NotEmpty().WithMessage(AppMessages.Application_Validator_Required)
+             //.NotEmpty().WithMessage(AppMessages.Application_Validator_Required)
            .MaximumLength(300).WithMessage(string.Format(AppMessages.Application_Validator_MaxLength, 300));
 
             RuleFor(request => request.Catalog.CatalogRequest.StatusId)
