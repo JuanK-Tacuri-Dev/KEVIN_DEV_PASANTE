dd4fa30b5e5e71a82989558aab45f5fa48d873ce
andresfgonzalez <infraestructura-qs@hotmail.com>
2024-10-02 15:57:14 -0500

    Se realizan ajustes pertinentes de para realizar sincronización.

--- Integration.Orchestrator.Backend.Domain/Entities/Administration/Interfaces/ISynchronizationStatesService.cs
+++ Integration.Orchestrator.Backend.Domain/Entities/Administration/Interfaces/ISynchronizationStatesService.cs
@@ -8,6 +8,7 @@ namespace Integration.Orchestrator.Backend.Domain.Entities.Administration.Interf
         Task UpdateAsync(T synchronizationStates);
         Task DeleteAsync(T synchronizationStates);
         Task<T> GetByIdAsync(Guid id);
+        Task<T> GetByCodeAsync(string code);
         Task<IEnumerable<T>> GetAllPaginatedAsync(PaginatedModel paginatedModel);
         Task<long> GetTotalRowsAsync(PaginatedModel paginatedModel);
     }
