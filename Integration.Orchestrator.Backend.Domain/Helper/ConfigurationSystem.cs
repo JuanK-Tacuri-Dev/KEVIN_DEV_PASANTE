6068f599ceff291cee62e591f3cd7c4b9134623f
Juan Carlos Tacuri <jtacuri@rp3.com.ec>
2024-10-21 11:22:52 -0500

    feat: [UPD] Set time zone  TEST

--- Integration.Orchestrator.Backend.Domain/Helper/ConfigurationSystem.cs
+++ Integration.Orchestrator.Backend.Domain/Helper/ConfigurationSystem.cs
@@ -1,23 +1,29 @@
-﻿namespace Integration.Orchestrator.Backend.Domain.Helper
+﻿using System;
+
+namespace Integration.Orchestrator.Backend.Domain.Helper
 {
     public static class ConfigurationSystem
     {
         public static string DateTimeFormat { get; set; } = "yyyy-MM-ddTHH:mm:ss.fffZ";
-        //public static string DateTimeDefault { get; set; } = GetTimeDefault();
+        public static string RegionZone { get; set; } = "Ecuador Time";
 
         public static string DateTimeDefault()
         {
             try
             {
-                return DateTime.Now.ToString(ConfigurationSystem.DateTimeFormat);
+                DateTime currentTime = DateTime.Now;
+                TimeZoneInfo TimeZone = TimeZoneInfo.FindSystemTimeZoneById(RegionZone);
+                DateTime DateRegion = TimeZoneInfo.ConvertTime(currentTime, TimeZoneInfo.Local, TimeZone);
+
+                return DateRegion.ToString(ConfigurationSystem.DateTimeFormat);
             }
             catch (TimeZoneNotFoundException)
             {
-                return DateTime.Now.ToString(ConfigurationSystem.DateTimeFormat); 
+                return DateTime.Now.ToString(ConfigurationSystem.DateTimeFormat);
             }
             catch (InvalidTimeZoneException)
             {
-                return DateTime.Now.ToString(ConfigurationSystem.DateTimeFormat); 
+                return DateTime.Now.ToString(ConfigurationSystem.DateTimeFormat);
             }
         }
 
