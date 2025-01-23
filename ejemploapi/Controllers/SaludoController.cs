e71a9ee69404ad7dcb905b48988382f62b903f83
Kevin272001 <kevin27carvajalg@gmail.com>
2025-01-23 10:48:34 -0500

    [INIT] Primer Api

---
+++ ejemploapi/Controllers/SaludoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiApiSencilla.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SaludoController : ControllerBase
+    {
+        // GET: api/Saludo
+        [HttpGet]
+        public IActionResult GetSaludo()
+        {
+            return Ok("¡Hola, mundo!");
+        }
+        // POST: api/Saludo
+        [HttpPost]
+        public IActionResult PostSaludo([FromBody] string nombre)
+        {
+            return Ok($"¡Saludos, {nombre}!");
+        }
+
+        // GET: api/Saludo/{nombre}
+        [HttpGet("{nombre}")]
+        public IActionResult GetSaludoPersonalizado(string nombre)
+        {
+            return Ok($"¡Hola, {nombre}!");
+        }
+    }
+}
